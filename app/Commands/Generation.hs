fc2230cfeb796fccc0ac66ca020252a99b11d1c8
rendick <rendick123@proton.me>
2024-08-21 03:41:50 +0300

    feat: a new function that generates the a new password and calculates the password strength using the entropy formula

--- app/Commands/Generation.hs
+++ app/Commands/Generation.hs
@@ -1,5 +1,29 @@
 module Commands.Generation where
 
-generation :: IO()
-generation = do
- putStrLn "ok"
+import System.Random
+import System.IO (hFlush, stdout)
+
+randomChar :: IO Char
+randomChar = getStdRandom $ randomR ('0', 'z')
+
+generation :: Int -> IO [Char]
+generation 0 = return ""
+generation n = randomChar >>= \c -> generation (n-1) >>= \b -> return $ [c] ++ b
+
+passwordEntropy :: Int -> Double
+passwordEntropy n = fromIntegral n * logBase 2 94
+
+runD :: IO ()
+runD = do 
+ let entropy = passwordEntropy 16
+ pwd <- generation 16
+ putStrLn $ pwd ++ " " ++ "[" ++ show entropy ++ "]"
+ putStr "Do you want to save this password? [Y/n] "
+ hFlush stdout
+ savePwdResult <- getLine
+ if savePwdResult == "" 
+  then do
+   putStrLn "ok"
+  else do
+   putStrLn "not ok"
+   return()
