cf922ad403a333e2ba82d87de141061bf905c424
rendick <rendick123@proton.me>
2024-08-21 03:38:31 +0300

    feat: new arguments - gen, help and version

--- app/Main.hs
+++ app/Main.hs
@@ -2,7 +2,30 @@ module Main where
 
 import Commands.Generation
 
+import System.Environment (getArgs)
+import System.Exit (exitSuccess)
+
 main :: IO ()
 main = do
- putStrLn "Hello, Haskell! ppp"
- generation
+ args <- getArgs
+ result <- parse args
+ putStrLn $ tac result
+
+tac :: String -> String
+tac = unlines . reverse . lines
+
+parse :: [String] -> IO String
+parse ["help"] = usage >> exit
+parse ["gen"] = runD >> exit
+parse ["version"] = version >> exit
+parse []= usage >> exit
+parse fs = concat <$> mapM readFile fs
+
+usage :: IO ()
+usage = putStrLn "Usage: ppp [ARGUMENT]"
+
+version :: IO ()
+version = putStrLn "v0.1.0 in development"
+
+exit :: IO a
+exit = exitSuccess
